447c875139b0def4d11227b9cd6864a901a81b5a
harsh <64204375+harsh2971@users.noreply.github.com>
2026-09-05 22:56:35 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,27 @@
+class MinStack {
+public:
+    stack<pair<int,int>>stk;// first to store actual value, second to store min value
+    MinStack() {
+    }
+    
+    void push(int val) {
+        if(stk.empty()){
+            stk.push({val,val});
+        }
+        else{
+            stk.push({val,min(val,stk.top().second)});
+        }
+    }
+    
+    void pop() {
+       stk.pop();
+    }
+    
+    int top() {
+      return stk.top().first;
+    }
+    
+    int getMin() {
+        return stk.top().second;
+    }
+};
